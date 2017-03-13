fa698e5243731d636379167884d36f18740ba376
MisterCodePL <mistercode11@gmail.com>
2017-03-13 19:09:23 +0100

    Added jumping

---
+++ Moon Problem/Assets/Scripts/CameraMovementScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public float MaxVerticalOffset = 3.0f;
+    public float ChangingVerticalPositionPerFrame = 0.075f;
+    private Transform _transform;
+    void Start ()
+    {
+        _transform = GetComponent<Transform>();
+    }
+	
+
+	void LateUpdate ()
+	{
+	    Vector3 position = _transform.position;
+	    position.x = PlayerTransform.position.x;
+	    if (PlayerTransform.position.y - position.y > MaxVerticalOffset)
+	    {
+            position.y += ChangingVerticalPositionPerFrame;
+	    }
+
+        if (PlayerTransform.position.y - position.y < -MaxVerticalOffset)
+        {
+            position.y -= ChangingVerticalPositionPerFrame;
+        }
+
+        _transform.position = position;
+	}
+}
--- Moon Problem/Assets/Scripts/MovementScript.cs
+++ Moon Problem/Assets/Scripts/MovementScript.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovementScript : MonoBehaviour {
     public float MovementSpeed = 0.125f;
+    public float JumpForce = 500f;
     private Transform _transform;
     private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
     private bool _facingRight = true;
+    private bool _canJump = true;
 
     public void Start()
     {
         _transform = gameObject.GetComponent<Transform>();
         _animator = gameObject.GetComponent<Animator>();
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
     public void Update()
     {
@@ -25,6 +26,29 @@ public class MovementScript : MonoBehaviour {
     public void FixedUpdate()
     {
         Move();
+        Jump();
+        Restart();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collide")
+        {
+            Vector3 position= _transform.position;
+            position.y -= 0.1f;
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, 0.001f);
+            if (hit.collider != null) _canJump = true;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && _canJump)
+        {
+            _rigidbody2D.AddForce(transform.up*JumpForce,ForceMode2D.Impulse);
+            _canJump = false;
+        }
+
     }
 
     private void Move()
@@ -47,5 +71,14 @@ public class MovementScript : MonoBehaviour {
         Vector3 scale = _transform.localScale;
         scale.x *= -1;
         _transform.localScale = scale;
+    }
+
+    private void Restart()
+    {
+        if (_transform.position.y < -20)
+        {
+            Vector3 position = new Vector3(0,0);
+            _transform.position = position;
+        }
     }
 }
