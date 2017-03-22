91632319f5ef60a81b509d8509674fc520030001
MisterCodePL <mistercode11@gmail.com>
2017-03-22 17:33:34 +0100

    Added movement to spider.

---
+++ Moon Problem/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+    public float MovementSpeed = 0.125f;
+    public float JumpForce = 30f;
+    private Transform _transform;
+    private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
+    private bool _facingRight = true;
+    private bool _canJump = true;
+
+    public void Start()
+    {
+        _transform = gameObject.GetComponent<Transform>();
+        _animator = gameObject.GetComponent<Animator>();
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && _facingRight) Flip();
+        if(Input.GetKey(KeyCode.RightArrow) && !_facingRight) Flip();
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) _animator.SetBool("move", true);
+        if(!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) _animator.SetBool("move", false);
+    }
+
+    public void FixedUpdate()
+    {
+        Move();
+        Jump();
+        Restart();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collide")
+        {
+            Vector3 position= _transform.position;
+            position.y -= 0.1f;
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, 0.001f);
+            if (hit.collider != null) _canJump = true;
+        }
+
+        if (collision.gameObject.tag == "Respawn")
+        {
+            Vector3 position = new Vector3(0, 0, 0);
+            _transform.position = position;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && _canJump)
+        {
+            _rigidbody2D.AddForce(transform.up*JumpForce,ForceMode2D.Impulse);
+            _canJump = false;
+        }
+
+    }
+
+    private void Move()
+    {
+        Vector3 position = _transform.position;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += MovementSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= MovementSpeed;
+        }
+        _transform.position = position;
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 scale = _transform.localScale;
+        scale.x *= -1;
+        _transform.localScale = scale;
+    }
+
+    private void Restart()
+    {
+        if (_transform.position.y < -20)
+        {
+            Vector3 position = new Vector3(0,0);
+            _transform.position = position;
+        }
+    }
+}
---
+++ Moon Problem/Assets/Scripts/SpiderMovementScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovementScript : MonoBehaviour {
+
+    public float MovementSpeed = 0.125f;
+    private Transform _transform;
+    private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
+    public bool _facingRight = false;
+    // Use this for initialization
+    void Start ()
+    {
+        _transform = gameObject.GetComponent<Transform>();
+        _animator = gameObject.GetComponent<Animator>();
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 scale = _transform.localScale;
+        scale.x *= -1;
+        _transform.localScale = scale;
+    }
+
+    private void Move()
+    {
+        Vector3 position = _transform.position;
+        if (_facingRight)
+        {
+            position.x += MovementSpeed;
+        }
+        if (!_facingRight)
+        {
+            position.x -= MovementSpeed;
+        }
+        _transform.position = position;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collide")
+        {
+            Vector3 position = _transform.position;
+            position.y += 1f;
+            position.x -= 2f;
+            RaycastHit2D hitLeft = Physics2D.Raycast(position, Vector2.left, 0.001f);
+            if (hitLeft.collider != null && hitLeft.collider.gameObject.tag != "Player") Flip();
+            position.x += 4f;
+            RaycastHit2D hitRight = Physics2D.Raycast(position, Vector2.right, 0.001f);
+            if (hitRight.collider != null && hitRight.collider.gameObject.tag != "Player") Flip();
+        }
+    }
+}
