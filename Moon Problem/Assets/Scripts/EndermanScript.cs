cdc30c8540a5a2a56b8586fd4b293fd3d352b88e
MisterCodePL <mistercode11@gmail.com>
2017-04-14 11:46:15 +0200

    added movement to enderman

--- Moon Problem/Assets/Scripts/EndermanScript.cs
+++ Moon Problem/Assets/Scripts/EndermanScript.cs
@@ -1,47 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class SpiderMovementScript : MonoBehaviour {
-
+public class EndermanScript : MonoBehaviour
+{
     public float MovementSpeed = 0.125f;
     private Transform _transform;
     private Animator _animator;
     private Collider2D _collider2D;
     public bool FacingRight = false;
-    public void Start ()
+
+    public void Start()
     {
         _transform = gameObject.GetComponent<Transform>();
         _animator = gameObject.GetComponent<Animator>();
         _collider2D = gameObject.GetComponent<Collider2D>();
     }
-	
-	public void FixedUpdate()
+
+    public void FixedUpdate()
     {
         Move();
         PhysicalElementOfDeathAnimation();
     }
 
-    public void OnCollisionStay2D(Collision2D collision)
+    private void Move()
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            var collisionDetector = new CollisionDetector(_collider2D);
-            if (collisionDetector.CollideOnTheTop() != null && 
-                collisionDetector.CollideOnTheTop().gameObject.tag == "Player") Die();
-        }
+        Vector3 position = _transform.position;
+        if(FacingRight) position.x += MovementSpeed;
+        if (!FacingRight) position.x -= MovementSpeed;
+        _transform.position = position;
     }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            var collisionDetector = new CollisionDetector(_collider2D);
-            if (collisionDetector.CollideOnTheLeft() != null &&
-                collisionDetector.CollideOnTheLeft().gameObject.tag == "Ground") Flip();
-            if (collisionDetector.CollideOnTheRight() != null && 
-                collisionDetector.CollideOnTheRight().gameObject.tag == "Ground") Flip();
-        }
-    }
-
 
     private void PhysicalElementOfDeathAnimation()
     {
@@ -66,6 +54,18 @@ public class SpiderMovementScript : MonoBehaviour {
         }
     }
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            var collisionDetector = new CollisionDetector(_collider2D);
+            if (collisionDetector.CollideOnTheLeft() != null &&
+                collisionDetector.CollideOnTheLeft().gameObject.tag == "Ground") Flip();
+            if (collisionDetector.CollideOnTheRight() != null &&
+                collisionDetector.CollideOnTheRight().gameObject.tag == "Ground") Flip();
+        }
+    }
+
     private void Flip()
     {
         FacingRight = !FacingRight;
@@ -74,18 +74,15 @@ public class SpiderMovementScript : MonoBehaviour {
         _transform.localScale = scale;
     }
 
-    private void Move()
+    public void OnCollisionStay2D(Collision2D collision)
     {
-        Vector3 position = _transform.position;
-        if (FacingRight)
+        if (collision.gameObject.tag == "Player")
         {
-            position.x += MovementSpeed;
+            var collisionDetector = new CollisionDetector(_collider2D);
+            if (collisionDetector.CollideOnTheTop() != null &&
+                collisionDetector.CollideOnTheTop().gameObject.tag == "Player") Die();
         }
-        if (!FacingRight)
-        {
-            position.x -= MovementSpeed;
-        }
-        _transform.position = position;
+
     }
 
     private void Die()
@@ -96,9 +93,11 @@ public class SpiderMovementScript : MonoBehaviour {
 
     private void DeathAnimation()
     {
+        _animator.SetTrigger("isDeath");
         Vector3 scale = _transform.localScale;
         scale.x *= 0.5f;
         scale.y *= 0.5f;
         _transform.localScale = scale;
     }
 }
+
