a3e609fa9e1dfbd0b34d19e94fe84306bf3bab03
MisterCodePL <mistercode11@gmail.com>
2017-04-22 17:52:04 +0200

    Added mine behavior

--- Moon Problem/Assets/Scripts/MineScript.cs
+++ Moon Problem/Assets/Scripts/MineScript.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class MineScript : MonoBehaviour
 {
@@ -6,6 +7,8 @@ public class MineScript : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private bool _facingRight;
     private Vector2 _direction;
+    public float TimeToDetonate = 1f;
+    private float _actualTimeToDetonate = 0;
     public void Start()
     {
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
@@ -47,8 +50,25 @@ public class MineScript : MonoBehaviour
 
     private void Flip()
     {
-        var size = _rigidbody2D.transform.position;
+        var size = _rigidbody2D.transform.localScale;
         size.x *= -1;
-        _rigidbody2D.transform.position = size;
+        _rigidbody2D.transform.localScale = size;
+    }
+
+    private void Update()
+    {
+        _actualTimeToDetonate += Time.fixedDeltaTime;
+        if (_actualTimeToDetonate > 1)
+        {
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(_rigidbody2D.transform.position, 2.5f)
+                .Where(t => t.tag=="Player" || t.tag=="Enemy")
+                .ToArray();
+            
+            foreach (var o in hitColliders)
+            {
+                Debug.Log(o);
+                Destroy(o.gameObject);
+            }
+        }
     }
 }
---
+++ Moon Problem/Assets/Scripts/MineSpawnerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSpawnerScript : MonoBehaviour
+{
+    public int NumberOfMines = 0;
+    public float DelayBetweenMines = 1.0f;
+    public GameObject MinePrefab;
+    private float _actualDelay = 0;
+
+	void Start ()
+    {
+		
+	}
+
+    void Update()
+    {
+        var playerPosition = FindPlayerPosition();
+        _actualDelay += Time.fixedDeltaTime;
+        if (_actualDelay > DelayBetweenMines && NumberOfMines>0 && IsInRange(playerPosition))
+        {
+            NumberOfMines--;
+            _actualDelay = 0;
+            var mine = (GameObject)Instantiate(
+                MinePrefab,GetSpawnPosition(), gameObject.transform.rotation);
+        }
+    }
+
+    private Vector2 FindPlayerPosition()
+    {
+        return GameObject.Find("Player").transform.position;
+    }
+
+    private bool IsInRange(Vector2 playerPosition)
+    {
+        var position = gameObject.transform.position;
+        var x = playerPosition.x - position.x;
+        if (x < 0) x = -x;
+        return x < 7.5f;
+    }
+
+    private Vector2 GetSpawnPosition()
+    {
+        Vector2 vector2 = new Vector2(gameObject.transform.position.x,gameObject.transform.position.y+1.25f);
+        return vector2;
+    }
+
+}
