213965b15d20e0aaf163bb109f315970a5e7929e
MisterCodePL <mistercode11@gmail.com>
2017-05-20 19:22:03 +0200

    updated Level1, added DialogEvent

---
+++ Moon Problem/Assets/Scripts/DialogEvent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang.Runtime.DynamicDispatching;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogEvent : MonoBehaviour
+{
+    [TextArea]
+    public string Text;
+
+    public Text Label;
+    public float DisplayTime;
+
+    private float _x;
+    private float _playerX;
+    private float _actualTime;
+
+    void Start()
+    {
+        _actualTime = 0;
+    }
+
+    void Update()
+    {
+        if(IsDisplay()) _actualTime += Time.fixedDeltaTime;
+        GetActualPositions();
+        if (InRange() && IsNotExceededTime() && !IsDisplay())
+        {
+            Label.text = Text;
+        }
+        if (!IsNotExceededTime())
+        {
+            Label.text="";
+            Destroy(gameObject.GetComponent<DialogEvent>());
+        }
+    }
+
+    private bool IsDisplay()
+    {
+        return Label.text == Text;
+    }
+
+    private void GetActualPositions()
+    {
+        _x = GetComponent<Transform>().position.x;
+        _playerX = GameObject.Find("Player").transform.position.x;
+    }
+
+    private bool InRange()
+    {
+        return Mathf.Abs(_playerX - _x) < 1f;
+    }
+
+    private bool IsNotExceededTime()
+    {
+        return _actualTime < DisplayTime;
+    }
+}
