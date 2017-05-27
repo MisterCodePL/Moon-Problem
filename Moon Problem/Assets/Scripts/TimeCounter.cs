be0e840f4c13b8c83a83d2f290096ef8d3af7e52
MisterCodePL <mistercode11@gmail.com>
2017-05-27 23:57:57 +0200

    added time counting

---
+++ Moon Problem/Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    private Text _text;
+    private float _timeInSeconds;
+
+
+    private void Start()
+    {
+        _text = GetComponentInChildren<Text>();
+        _timeInSeconds = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        _timeInSeconds += Time.fixedDeltaTime;
+        UpdateDisplayClock();
+    }
+
+    private void UpdateDisplayClock()
+    {
+        int minutes = (int)(_timeInSeconds / 60);
+        int seconds = (int)_timeInSeconds - minutes * 60;
+        string secondsText = seconds.ToString();
+        if (seconds < 10) secondsText = "0" + seconds;
+        _text.text = minutes + ":" + secondsText;
+    }
+}
