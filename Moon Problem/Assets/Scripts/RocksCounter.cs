f364a951526e2cc44bce3f1055206b2760578f1d
MisterCodePL <mistercode11@gmail.com>
2017-05-27 17:29:48 +0200

    added rocks counter

---
+++ Moon Problem/Assets/Scripts/RocksCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocksCounter : MonoBehaviour
+{
+    private int _rocks;
+    private Text _text;
+
+    private void Start()
+    {
+        _rocks = 0;
+        _text = GetComponentInChildren<Text>();
+    }
+
+    public void AddRock()
+    {
+        _rocks++;
+        _text.text = _rocks.ToString();
+    }
+
+
+}
