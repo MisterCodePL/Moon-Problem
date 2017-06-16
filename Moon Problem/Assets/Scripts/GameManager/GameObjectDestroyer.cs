80d09abdd1ea6725ef1a68b84da278206bff5c28
MisterCodePL <mistercode11@gmail.com>
2017-06-16 15:35:23 +0200

    refactor

---
+++ Moon Problem/Assets/Scripts/GameManager/GameObjectDestroyer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+class GameObjectDestroyer : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        DestroyDeathGameObject();
+    }
+
+    private void DestroyDeathGameObject()
+    {
+        var deathGameObjects = FindAllDeathGameObjects();
+        foreach (var o in deathGameObjects)
+        {
+            Destroy(o);
+        }
+    }
+
+    private List<GameObject> FindAllDeathGameObjects()
+    {
+        List<GameObject> list = new List<GameObject>();
+        try
+        {
+            list = GameObject.FindGameObjectsWithTag("Enemy").ToList();
+            list.Add(GameObject.Find("Player"));
+            list = list.Select(x => x)
+                .Where(t => t.transform.position.y < -7.5f)
+                .ToList();
+        }
+        catch (NullReferenceException)
+        {
+            ;
+        }
+        return list;
+    }
+}
